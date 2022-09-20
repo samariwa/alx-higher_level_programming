361b1583a66acff80f78253f908be11bb87ef468
samariwa <samuelmariwa@gmail.com>
2022-09-20 15:35:12 +0000

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(string):
 	result = ''
 	for c in range(0, len(string)):
         	if ord(string[c]) >= 97 and ord(string[c]) <= 122:
-                	result += "{:s}".format(ord(string[c]))
+                	result += chr(ord(string[c]) - 32)
         	else:
-                	result += "{:s}".format(ord(string[c]) - 32)
+                	result += string[c]
 
 	print(result)
