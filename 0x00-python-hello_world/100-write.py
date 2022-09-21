63a32d63a5b72eb6b2bcd605b1633a6f2e592f32
samariwa <samuelmariwa@gmail.com>
2022-09-21 06:22:27 +0000

    Pycodestyling

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 import sys
 from datetime import datetime
 
 date = datetime(2015, 10, 19)
 def main():
-	sys.stderr.write(f'and that piece of art is useful - Dora Korpar, {date: %Y-%m-%d}\n')
-	exit(1)
+    sys.stderr.write(f'and that piece of art is useful - Dora Korpar, {date: %Y-%m-%d}\n')
+    exit(1)
   
 
 if __name__=="__main__":
