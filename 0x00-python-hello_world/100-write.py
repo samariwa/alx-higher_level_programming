66e62786830db10b1b6bf206b75ce186a43c0e67
samariwa <samuelmariwa@gmail.com>
2022-09-21 06:28:19 +0000

    Pycodestyling

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,8 +3,11 @@ import sys
 from datetime import datetime
 
 date = datetime(2015, 10, 19)
+
+
 def main():
-    sys.stderr.write(f'and that piece of art is useful - Dora Korpar, {date: %Y-%m-%d}\n')
+    sys.stderr.write(f'and that piece of art is useful -\ 
+    Dora Korpar, {date: %Y-%m-%d}\n')
     exit(1)
   
 
