88ee95006eed7c7d32b2fb637821d73bb9458315
samariwa <samuelmariwa@gmail.com>
2022-09-22 17:02:58 +0000

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,9 +5,9 @@ def add_result():
     a = 1
     b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     add_result()
