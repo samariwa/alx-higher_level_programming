6872a40cde275d0cd8dbdceae530a272904da08d
samariwa <samuelmariwa@gmail.com>
2022-09-24 17:48:26 +0000

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    count = 0
+
+    for i in range(len(sentence)):
+	    count += 1
+    result = ()
+    result = (count, sentence[0],)
+
+    return(result)
