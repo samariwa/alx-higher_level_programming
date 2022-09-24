d04a205fc08ecbd9cd3da826fe925a2154f0ade0
samariwa <samuelmariwa@gmail.com>
2022-09-24 17:16:39 +0000

    Matrix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,5 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
-        print('\n')
+    for line in matrix:
+        print ('  '.join(map(str, line)))
