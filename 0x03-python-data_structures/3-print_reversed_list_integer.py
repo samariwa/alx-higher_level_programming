e6ce92521afc336017ad8050dd9248591fee6814
samariwa <samuelmariwa@gmail.com>
2022-09-28 01:58:24 +0000

    Reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     my_list.reverse()
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
