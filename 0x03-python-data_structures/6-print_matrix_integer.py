73c4597db765037bdf1ed7c09a929d0d7378cccc
samariwa <samuelmariwa@gmail.com>
2022-09-28 01:49:27 +0000

    Pycodestyling

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,6 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for val in line:
-            print ("{:s}{:d}".format('' if line.index(val) == 0 else ' ', val), end='')
+            print("{:s}{:d}".format('' if line.index(val) == 0
+                                    else ' ', val), end='')
         print()
