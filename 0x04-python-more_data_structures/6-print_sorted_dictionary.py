fa3eeb1f86c379472510c41a30a768701700b059
samariwa <samuelmariwa@gmail.com>
2022-09-28 12:49:21 +0000

    Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_matrix = sorted(a_dictionary.items())
+
+    for line in sorted_matrix:
+        print("{}: {}".format(line[0], line[1]))        
