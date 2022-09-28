4a57aab6ded0353dc133f6a2518a08a6fc5e93a8
samariwa <samuelmariwa@gmail.com>
2022-09-28 18:31:51 +0000

    Delete by value

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    new_matrix = []
+
+    for line in matrix: 
+        new_matrix.append(list(map(lambda x: x ** 2, line)))
+
+    return(new_matrix)
+        
+	    
