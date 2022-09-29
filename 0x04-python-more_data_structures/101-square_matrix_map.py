d140bbf2d98330924fea58ba2a33cc4ee73d4863
samariwa <samuelmariwa@gmail.com>
2022-09-29 02:17:25 +0000

    Pycodestyling

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,9 +2,7 @@
 def square_matrix_map(matrix=[]):
     new_matrix = []
 
-    for line in matrix: 
+    for line in matrix:
         new_matrix.append(list(map(lambda x: x ** 2, line)))
 
     return(new_matrix)
-        
-	    
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     sorted_matrix = sorted(a_dictionary.items())
 
     for line in sorted_matrix:
-        print("{}: {}".format(line[0], line[1]))        
+        print("{}: {}".format(line[0], line[1]))
