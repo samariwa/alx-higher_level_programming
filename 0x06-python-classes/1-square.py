e855f2d8c0d7ba6bc948d0e833c153dd9186907a
samariwa <samuelmariwa@gmail.com>
2022-10-13 08:20:45 +0000

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""a module for a square class with private args
+""" Creating a class """
 
-Example:
-    This module is expected to have a Class with private args
-        $ ./1-main.py
-        <class '1-square.Square'>
-        {'_Square__size': 3}
-        'Square' object has no attribute 'size'
-        'Square' object has no attribute '__size'
-"""
 
 class Square:
-    """a class Square that defines a square
-    private instance attribute: size
-    Instantiation with size
-    """
+    """ creates an a clas swith an private attribute size """
     def __init__(self, size):
-	"""initialization of the square object
-
-        Args:
-            __size (int): length of the square which is a private attribute
-        """
+        """ Initialize the instance """
         self.__size = size
