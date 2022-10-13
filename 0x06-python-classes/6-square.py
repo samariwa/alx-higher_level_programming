2b55edd073036c48021740f6da8ed10886f42c0f
samariwa <samuelmariwa@gmail.com>
2022-10-13 08:47:07 +0000

    Documentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Square class creation"""
+""" Square class creation """
 
 class Square:
-    """Constructor of the square class"""
+    """ Constructor of the square class """
     def __init__(self, size=0, position=(0, 0)):
-        """setting size and position with type and value checkers"""
+        """ setting size and position with type and value checkers """
         if size < 0:
             raise ValueError("size must be >= 0")
         elif type(size) is not int:
@@ -13,7 +13,7 @@ class Square:
             self.__size = size
 
     def area(self):
-	"""returns the area of the square"""
+	""" returns the area of the square """
         return (self.__size ** 2)
 
     def my_print(self):
@@ -30,7 +30,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """setting size with type and value checkers"""
+        """ setting size with type and value checkers """
         if value < 0:
             raise ValueError("size must be >= 0")
         elif type(value) is not int:
@@ -40,12 +40,12 @@ class Square:
 
     @property
     def position(self):
-        """returns the position of the square"""
+        """ returns the position of the square """
         return (self.__position)
 
     @position.setter
     def position(self, value):
-        """setting size with type and value checkers"""
+        """ setting size with type and value checkers """
         for point in value:
             if type(point) is int or point >= 0:
                 self.__position = value
