fef92eec16c11e5ee0c7cccf2aa6a91f0edbcd97
samariwa <samuelmariwa@gmail.com>
2022-10-13 06:17:45 +0000

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,16 +3,22 @@
    and initialization of the size priperty in the class
    constructor
 
-   Example: This module will have a class with private args
-   $ ./1-main.py
-   <class '1-square.Square'>
-   {'_Square__size': 3}
-   'Square' object has no attribute 'size'
-   'Square' object has no attribute '__size'
+Example: This module will have a class with private args
+    $ ./1-main.py
+    <class '1-square.Square'>
+    {'_Square__size': 3}
+    'Square' object has no attribute 'size'
+    'Square' object has no attribute '__size'
 """
 
 class Square:
-    """constructor of the square class"""
+    """constructor of the square class
+       private instance attribute: size
+       Instantiation with size
+    """
     def __init__(self, size):
-	"""initialization of the private attribute size"""
+	"""initialization of the square object
+           Args:
+               _size (int): length of the square which is a private attribute
+        """
         self._size = size
