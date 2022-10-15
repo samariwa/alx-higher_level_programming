d9353b77ff1b66ad99b41c184e45e8dc38a72d22
samariwa <samuelmariwa@gmail.com>
2022-10-15 13:48:02 +0000

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,9 @@ class Square:
     def __init__(self, size=0):
         """initialization of the private attribute\
         size based on input checker"""
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        elif type(size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
