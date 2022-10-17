530cd2504d46bd354dc18f575c9363cdfe50981b
samariwa <samuelmariwa@gmail.com>
2022-10-17 17:11:34 +0000

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -44,6 +44,13 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area() or rect_1.area() == rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     def area(self):
         """ returns the area of the rectangle """
