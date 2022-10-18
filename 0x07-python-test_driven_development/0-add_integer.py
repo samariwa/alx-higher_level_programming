578d0f64492dda9234609643538265925b114a96
samariwa <samuelmariwa@gmail.com>
2022-10-18 12:55:47 +0000

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    """ function that adds 2 integers """
-    return (a + b)
+    """ function that adds 2 integers\
+    checkers for value input are also set"""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
