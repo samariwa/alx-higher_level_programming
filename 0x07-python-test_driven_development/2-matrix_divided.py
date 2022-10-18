142ca97b43c3e2ad35b59850a90982f4e9b24a94
samariwa <samuelmariwa@gmail.com>
2022-10-18 13:56:15 +0000

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,10 @@
 
 def matrix_divided(matrix, div):
     """ Function that divides value in a matrix by specified value"""
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif type(div) is not int:
+        raise TypeError("div must be a number")
     result_matrix = []
     for row in matrix:
         new_row = []
