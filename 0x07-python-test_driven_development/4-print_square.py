04e803f466c54f8d699a17fe5cbf0185b8662c8b
samariwa <samuelmariwa@gmail.com>
2022-10-19 17:24:06 +0000

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,6 +4,12 @@
 def print_square(size):
     """ Function that prints the square with the # character\
     based on the size passed as a parameter """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float:
+        raise TypeError("size must be an integer")
     for width in range(size):
         for length in range(size):
             print('#', end='')
