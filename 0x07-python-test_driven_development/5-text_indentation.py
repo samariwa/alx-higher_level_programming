24d9e48222bc3c36d0a315e6605a1145b444753f
samariwa <samuelmariwa@gmail.com>
2022-10-19 17:59:57 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,10 @@
 
 
 def text_indentation(text):
+    """ function that prints a text with 2 new lines after\
+    each of these characters: ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
     breakers = ['.', '?', ':']
     new_text = ''
 
