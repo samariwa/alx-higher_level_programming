7cbdddcd14847801340f0481f6132e2af20970e8
samariwa <samuelmariwa@gmail.com>
2022-10-24 22:17:06 +0300

    Square subclass

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" This is a module that contains a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ This is the BaseGeometry class. It is the super class of the module """
+    def area(self, *args):
+        pass
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().integer_validator(width, width)
+        self.__width = width
+        super().integer_validator(height, height)
+        self.__height = height
+
+    def print(self, value):
+        print(value)
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+class Square(BaseGeometry):
+    def __init__(self, size):
+        super().integer_validator(size, size)
+        self.__size = size
+
+    def print(self, value):
+        print(value)
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
