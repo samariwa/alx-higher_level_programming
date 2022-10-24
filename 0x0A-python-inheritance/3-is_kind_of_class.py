75c4f95392675ae2b91229fa2ec9eeaadf7da695
samariwa <samuelmariwa@gmail.com>
2022-10-24 20:13:01 +0300

    Geometry module

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    return type(obj) is a_class
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+
+""" This is a module that contains a class BaseGeometry """
+class BaseGeometry:
+    """ This is the BaseGeometry class. It is the super class of the module """
+    pass
