dbcc70aa05c473ed5166907e7cc85bfe5a22749f
samariwa <samuelmariwa@gmail.com>
2022-10-25 07:11:48 +0000

    Module documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
+""" This module has a functions that checks if an object is an instance\
+of the specified class """
 
 def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
-
+    """ returns True if the object is exactly an instance of the specified\
+    class; otherwise False """
+    if type(obj) == a_class
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+""" This module contains a function that checks if a class is a subclass of§
+the specified superclass """
 
 def inherits_from(obj, a_class):
+    """ function that returns True if the object is an instance of a class\
+    that inherited (directly or indirectly) from the specified class;\
+    otherwise False """
     return isinstance(obj, a_class)
