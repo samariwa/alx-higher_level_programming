d9111d6455fd117385d32dfa3fccfb23293bbed3
samariwa <samuelmariwa@gmail.com>
2022-10-25 13:08:47 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module contains a function that reads an input file and\
+prints its content to the standard output """
+
+def read_file(filename=""):
+    """ this function opens the provided file and prints its content\
+    to stdout """
+    fd = open(filename, mode='r', encoding='UTF8')
+    print(fd.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Introduction to file I/O in Python
