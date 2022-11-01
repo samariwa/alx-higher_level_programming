12e1cd01f875cd9dca129a1f7a58ce8dd4800771
samariwa <samuelmariwa@gmail.com>
2022-11-01 17:47:14 +0000

    Update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,7 @@ class Square(Rectangle):
         if type(size) is not int:
             raise TypeError("width must be an integer")
         if size <= 0:
-            raise ValueError("width: must be > 0")
+            raise ValueError("width must be > 0")
         super().__init__(size, size, x, y, id)
         self.size = size
 
