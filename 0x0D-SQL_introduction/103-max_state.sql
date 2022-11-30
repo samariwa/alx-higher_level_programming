8327abe6bd3dfca93c43ffcc10217abf7195ffb3
“samariwa” <samuelmariwa@gmail.com>
2022-11-30 02:28:56 +0000

    Capitalization of keywords in SQL

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,2 @@
 -- displays the max temperature of each state
-SELECT state, MAX(value) as max_temp from temperatures GROUP BY state ORDER BY state ASC;
-
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
