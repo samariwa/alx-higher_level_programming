8afb2349bdc9de6f8c5c8f8cdd4ab01681318e23
“samariwa” <samuelmariwa@gmail.com>
2022-12-01 10:19:02 +0000

    Root User

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Script to create new user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
 
