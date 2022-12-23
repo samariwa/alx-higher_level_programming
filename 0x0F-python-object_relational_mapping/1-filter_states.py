14f38c19a5304369980d3c7e7efbaef0e60d46af
“samariwa” <samuelmariwa@gmail.com>
2022-12-23 05:17:25 +0000

    Documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,16 +6,22 @@
 import MySQLdb
 import sys
 
-username = "'"+sys.argv[1]+"'"
-password = "'"+sys.argv[2]+"'"
-database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost' , user=username, passwd=password, db=database)
+if __name__ == '__main__':
+    # establish connection to the database
+    username = "'"+sys.argv[1]+"'"
+    password = "'"+sys.argv[2]+"'"
+    database = sys.argv[3]
 
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', 
+		         user=username, 
+			 passwd=password, 
+			 db=database)
 
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER by id ASC")
+    cur = db.cursor()
 
-rows = cur.fetchall()
-for row in rows:
-    print("({}, '{}')". format(row[0], row[1]))
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER by id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print("({}, '{}')". format(row[0], row[1]))
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,18 +2,23 @@
 import MySQLdb
 import sys
 
-username = "'"+sys.argv[1]+"'"
-password = "'"+sys.argv[2]+"'"
-database = sys.argv[3]
+if __name__ == '__main__':
+    # establish connection to the database
+    username = "'"+sys.argv[1]+"'"
+    password = "'"+sys.argv[2]+"'"
+    database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost' , user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host='localhost', 
+		         user=username, 
+			 passwd=password, 
+			 db=database)
 
-cur = db.cursor()
+    cur = db.cursor()
 
-query = "SELECT * FROM states WHERE name = '"+sys.argv[4]+"' ORDER by id ASC"
+    qry = "SELECT * FROM states WHERE name = '"+sys.argv[4]+"' ORDER by id ASC"
 
-cur.execute(query)
+    cur.execute(qry)
 
-rows = cur.fetchall()
-for row in rows:
-    print("({}, '{}')". format(row[0], row[1]))
+    rows = cur.fetchall()
+    for row in rows:
+        print("({}, '{}')". format(row[0], row[1]))
