9bcc560ee691ac6a52ab613d22cb55006853a512
“samariwa” <samuelmariwa@gmail.com>
2022-12-23 05:00:14 +0000

    Documentation

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+# Usage: ./4-cities_by_state.py <mysql username> \
+#                               <mysql password> \
+#                               <database name>
 import MySQLdb
 import sys
 
