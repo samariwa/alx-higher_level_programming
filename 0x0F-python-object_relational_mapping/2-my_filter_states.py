8c29ef15b14a4d3e17d89604c20a15e4a16d33fb
“samariwa” <samuelmariwa@gmail.com>
2022-12-23 05:39:41 +0000

    Documentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
+"""
 import MySQLdb
 import sys
 
